c029cbd6eb5adb482877137cccc23cf668dfa2f3
Ali <ali.v.tovar@gmail.com>
2025-09-13 17:27:46 -0300

    mejora10

---
+++ utils/settings.py
@@ -0,0 +1,8 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+BASE_URL   = os.environ ["BASE_URL"]
+ADMIN_USER = os.environ ["ADMIN_USER"]
+ADMIN_PASS = os.environ ["ADMIN_PASSWORD"]
+
